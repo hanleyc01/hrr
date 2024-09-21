fd1185ac857f100724d4805587517b7f6d72d468
Mary Kelly <62857962+ecphory@users.noreply.github.com>
2024-09-20 20:49:37 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+        name='hrr',
+        version='0.0.1',
+        description='Defines a class/object for Holographic Reduced Representations',
+        author='Mary Kelly',
+        author_email='marykelly4@carleton.ca',
+        packages=['hrr'],
+        install_requires=['numpy'],
+
+        classifiers=[
+            'Programming Language :: Python :: 3'
+            ]
+)
